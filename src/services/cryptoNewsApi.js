e7abe1d93bf8488a0c25f4be6700e704076d65b3
Chirag-astro <117797626+Chirag-astro@users.noreply.github.com>
2024-09-26 18:24:09 +0530

    Update cryptoNewsApi.js

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,27 +1,27 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const cryptoNewsHeaders = {
+const cryptoNewsHeaders = {
 
-//     'X-RapidAPI-Key': '10ea1db8c7msh21719a9433a87d5p170cbdjsnb5a5f052c340',
-//     'X-RapidAPI-Host': 'cryptocurrency-news2.p.rapidapi.com'
-// };
+    'x-rapidapi-key': '10ea1db8c7msh21719a9433a87d5p170cbdjsnb5a5f052c340',
+    'x-rapidapi-host': 'cryptocurrency-news2.p.rapidapi.com'
+};
 
-// const baseUrl = 'https://cryptocurrency-news2.p.rapidapi.com';
+const baseUrl = 'https://cryptocurrency-news2.p.rapidapi.com';
 
-// const createRequest = (url) => ({ url, headers: cryptoNewsHeaders }); 
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders }); 
 
-// export const cryptoNewsApi = createApi({
-//     reducerPath: 'cryptoNewsApi', 
-//     baseQuery : fetchBaseQuery({baseUrl}),
-//     endpoints: (builder) => ({
-//         getCryptoNews: builder.query({
-//             query:(count) => createRequest(`/v1/coindesk?limit=${count}`)
-//         })
-//     })
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi', 
+    baseQuery : fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query:() => createRequest(`/v1/coindesk`)
+        })
+    })
 
-// });
+});
 
-// export const { useGetCryptoNewsQuery} =  cryptoNewsApi;
+export const { useGetCryptoNewsQuery} =  cryptoNewsApi;
 
 // // headers: {
 // //     'X-BingApis-SDK': 'true',
